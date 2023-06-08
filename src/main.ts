d4a74fd5b1c153c33d3f20c67a81705d60ceebb1
Ezekiel <ehalle4@students.towson.edu>
2023-06-08 12:18:10 -0400

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import {
+    Client,
+    GatewayIntentBits,
+    Collection,
+    PermissionFlagsBits,
+} from "discord.js";
+import { Command, SlashCommand } from "./types";
+import { config } from "dotenv";
+import { readdirSync } from "fs";
+import { join } from "path";
+
+const { Guilds, MessageContent, GuildMessages, GuildMembers } =
+    GatewayIntentBits;
+const client = new Client({
+    intents: [Guilds, MessageContent, GuildMessages, GuildMembers],
+});
+
+config();
+
+client.slashCommands = new Collection<string, SlashCommand>();
+client.commands = new Collection<string, Command>();
+client.cooldowns = new Collection<string, number>();
+
+const handlersDir = join(__dirname, "./handlers");
+readdirSync(handlersDir).forEach((handler) => {
+    if (handler != "Mongo.ts") {
+        require(`${handlersDir}/${handler}`)(client);
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
